aaa5cf9787347e6c933f3e40b7e91f1432395ace
Eren Ece <erenece595@gmail.com>
2026-07-14 22:39:34 +0300

    14 temmuz angular1

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+  
+  products:Product[] = [  ];
+
+}
