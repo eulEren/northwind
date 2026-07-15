2953c7154847e2dc926cbf6ee6ade3e191b28953
Eren Ece <erenece595@gmail.com>
2026-07-15 18:23:43 +0300

    15 temmuz angular

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
-export class CategoryComponent {
-
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  dataLoaded = false;
+  constructor(private categoryService: CategoryService) {}
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+      this.dataLoaded = true;
+    });
+  }
 }
