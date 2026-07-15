041dd30bfc77d37881f00433cd4207619fdc75d4
Eren Ece <erenece595@gmail.com>
2026-07-15 21:53:42 +0300

    15 temmuz 3

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -11,14 +12,32 @@ import { ProductService } from '../../services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+  ) {}
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if (params["categoryId"]) {
+        this.getProductsByCategoryId(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
   getProducts() {
     this.productService.getProducts().subscribe((response) => {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+
+  getProductsByCategoryId(categoryId: number) {
+    this.productService
+      .getProductsByCategoryId(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
   }
 }
